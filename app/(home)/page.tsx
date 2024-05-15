22ea8ee8fb2d33486dcded0e9c349b35b2eb8372
irwinekabakti <irwinekabakti@gmail.com>
2024-05-15 16:24:01 +0700

    add:(detail using modal)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import Category from "../_components/_Category/page";
 import Card from "../_components/_Card/page";
-// import { ProductProvider } from "@/hooks/ProductContext";
 
 const Home = () => {
   return (
-    // <ProductProvider>
     <main className="home">
       <Category />
       <Card />
     </main>
-    // </ProductProvider>
   );
 };
 
